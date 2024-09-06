e00c18fc569a6bfbe8cbf1752865398aa520155d
Zin Hoang <hoang.mantue@gmail.com>
2024-09-06 15:31:05 +1200

    add Header section

---
+++ src/sections/header/Header.styles.ts
@@ -0,0 +1,93 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styled from 'styled-components';
+
+export const HeaderWrapper = styled.header`
+  position: fixed;
+  display: flex;
+  align-items: center; // need to check if need both align-items and justify-content
+  justify-content: space-between;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1000; // this is to be removed if possible
+  background-color: ${({ theme }) => theme.backgroundColor};
+
+  @media (max-width: 991px) {
+    html {
+      font-size: 55%;
+    }
+    & {
+      padding: 1.5rem 2rem;
+    }
+  }
+
+  @media (max-width: 450px) {
+    html {
+      font-size: 50%;
+    }
+  }
+`;
+
+export const Logo = styled.span`
+  color: ${({ theme }) => theme.white};
+  font-size: 2.5rem;
+  font-weight: bolder;
+`;
+
+export const PaperPlaneIcon = styled(FontAwesomeIcon)`
+  color: ${({ theme }) => theme.blue};
+  padding-right: 0.5rem;
+`;
+
+export const CustomNav = styled.nav<{ $isActive: boolean }>`
+  & ul {
+    list-style-type: none;
+    display: flex;
+    gap: 1rem;
+
+    & li {
+      font-size: 1.7rem;
+      /* color: ${({ theme }) => theme.lightColor}; */
+      color: ${({ theme }) => theme.white};
+      /* display: inline-block; NO EFFECT*/
+      margin: 0 1rem;
+      &:hover {
+        color: ${({ theme }) => theme.blue};
+      }
+    }
+  }
+  @media (max-width: 768px) {
+    position: absolute;
+    top: 99%;
+    left: 0;
+    right: 0;
+    background-color: ${({ theme }) => theme.backgroundColor};
+
+    // collapse the menu when not active
+    clip-path: ${({ $isActive }) =>
+      $isActive
+        ? 'polygon(0 0, 100% 0, 100% 100%, 0 100%)'
+        : 'polygon(0 0, 100% 0, 100% 0, 0 0)'};
+
+    & ul {
+      flex-direction: column;
+      gap: 0;
+      & li {
+        margin: 2rem;
+        font-size: 2rem;
+        display: block;
+      }
+    }
+  }
+`;
+
+export const MenuButton = styled(FontAwesomeIcon)`
+  font-size: 2.5rem;
+  color: ${({ theme }) => theme.white};
+  cursor: pointer;
+  display: none;
+
+  @media (max-width: 768px) {
+    display: inline-block;
+  }
+`;
--- src/sections/header/Header.tsx
+++ src/sections/header/Header.tsx
@@ -1,7 +1,60 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from 'react';
+import { faPaperPlane, faBars } from '@fortawesome/free-solid-svg-icons';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import { About } from '../about/About';
+import {
+  HeaderWrapper,
+  Logo,
+  CustomNav,
+  PaperPlaneIcon,
+  MenuButton,
+} from './Header.styles';
+import { ButtonLink } from '../../styles/sharedStyles';
 
 export const Header = () => {
-  return <FontAwesomeIcon icon={faPaperPlane} />;
+  const [isActive, setIsActive] = useState<boolean>(false);
+  const handleClick = () => {
+    setIsActive(!isActive);
+  };
+
+  return (
+    <HeaderWrapper>
+      <MenuButton icon={faBars} onClick={handleClick} />
+      <Logo>
+        <PaperPlaneIcon icon={faPaperPlane} />
+        Triplify
+      </Logo>
+
+      <Router>
+        <CustomNav $isActive={isActive}>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/destination">Destination</Link>
+            </li>
+            <li>
+              <Link to="/services">Services</Link>
+            </li>
+            <li>
+              <Link to="/gallery">Gallery</Link>
+            </li>
+            <li>
+              <Link to="/blogs">Blog</Link>
+            </li>
+          </ul>
+        </CustomNav>
+        <Routes>
+          {/* <Route path="/" element={MainContent} /> */}
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
+
+      <ButtonLink>Book Now</ButtonLink>
+    </HeaderWrapper>
+  );
 };
