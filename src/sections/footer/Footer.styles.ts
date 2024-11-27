dfe1ae9db176e5e9e8410efdf1978fef4907a683
Zin Hoang <hoang.mantue@gmail.com>
2024-11-27 18:37:09 +1300

    Add Footer section

---
+++ src/sections/footer/Footer.styles.ts
@@ -0,0 +1,76 @@
+import { StyledIcon } from './../../styles/sharedStyledComponents/FontAwesomeIcon.styles';
+import styled from 'styled-components';
+
+export const FooterWrapper = styled.section`
+  margin-top: 5rem;
+`;
+
+export const ColumnContainer = styled.div`
+  padding: 1rem 0;
+  h3 {
+    font-size: 2.2rem;
+    color: ${({ theme }) => theme.white};
+    padding: 1rem 0;
+  }
+
+  p {
+    font-size: 1.4rem;
+    color: ${({ theme }) => theme.lightColor};
+    padding: 1rem 0;
+    line-height: 2;
+  }
+`;
+
+export const SocialMediaContainer = styled.div`
+  padding-top: 1rem;
+  display: flex;
+  a {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 3.5rem;
+    width: 3.5rem;
+    font-size: 1.7rem;
+    border-radius: 50%;
+    border: ${({ theme }) => theme.border};
+    color: ${({ theme }) => theme.brightAqua} !important;
+    margin-right: 0.75rem;
+    &:hover {
+      background-color: ${({ theme }) => theme.brightAqua};
+      color: ${({ theme }) => theme.darkBackgroundColor};
+    }
+  }
+`;
+
+export const QuickLinksContainer = styled.div`
+  font-size: 1.4rem;
+  color: ${({ theme }) => theme.lightColor};
+  padding: 1rem 0;
+  display: block;
+
+  a {
+    display: flex;
+    align-items: center;
+    margin-bottom: 2rem;
+
+    &:hover {
+      color: ${({ theme }) => theme.brightAqua};
+
+      ${StyledIcon} {
+        padding-right: 2rem;
+      }
+    }
+  }
+`;
+
+export const EmailInput = styled.input`
+  width: 100%;
+  border: ${({ theme }) => theme.border};
+  border-radius: 5rem;
+  padding: 1.2rem 1.4rem;
+  margin-bottom: 1.5rem;
+  font-size: 1.5rem;
+  color: ${({ theme }) => theme.lightColor};
+  background: none;
+  text-transform: none;
+`;
---
+++ src/sections/footer/Footer.tsx
@@ -0,0 +1,114 @@
+import React from 'react';
+import {
+  Button,
+  GridContainer,
+  Logo,
+  StyledIcon,
+} from '../../styles/sharedStyledComponents';
+import {
+  faPaperPlane,
+  faArrowRight,
+  faMap,
+  faPhone,
+  faEnvelope,
+  faClock,
+} from '@fortawesome/free-solid-svg-icons';
+import { FaFacebookF } from 'react-icons/fa';
+import { PiGithubLogoFill } from 'react-icons/pi';
+import { BiLogoLinkedin } from 'react-icons/bi';
+import { RiMessengerFill } from 'react-icons/ri';
+import {
+  ColumnContainer,
+  EmailInput,
+  FooterWrapper,
+  QuickLinksContainer,
+  SocialMediaContainer,
+} from './Footer.styles';
+
+export const Footer = () => {
+  return (
+    <FooterWrapper>
+      <GridContainer columnWidth="25rem">
+        <ColumnContainer>
+          <Logo fontSize="2.2rem">
+            <StyledIcon icon={faPaperPlane} paddingRight="0.5rem" />
+            Triplify
+          </Logo>
+          <p>Lorum teppu aoisdoasnd oiasnd</p>
+          <SocialMediaContainer>
+            <a href="#">
+              <FaFacebookF />
+            </a>
+            <a href="#">
+              <RiMessengerFill />
+            </a>
+            <a href="#">
+              <PiGithubLogoFill />
+            </a>
+            <a href="#">
+              <BiLogoLinkedin />
+            </a>
+          </SocialMediaContainer>
+        </ColumnContainer>
+
+        <ColumnContainer>
+          <h3>Quick Links</h3>
+          <QuickLinksContainer>
+            <a href="#home">
+              <StyledIcon icon={faArrowRight} paddingRight="0.8rem" /> Home
+            </a>
+            <a href="#about">
+              <StyledIcon icon={faArrowRight} paddingRight="0.8rem" /> About
+            </a>
+            <a href="#destination">
+              <StyledIcon icon={faArrowRight} paddingRight="0.8rem" />{' '}
+              Destination
+            </a>
+            <a href="#services">
+              <StyledIcon icon={faArrowRight} paddingRight="0.8rem" /> Services
+            </a>
+            <a href="#gallery">
+              <StyledIcon icon={faArrowRight} paddingRight="0.8rem" /> Gallery
+            </a>
+            <a href="#blogs">
+              <StyledIcon icon={faArrowRight} paddingRight="0.8rem" /> Blogs
+            </a>
+          </QuickLinksContainer>
+        </ColumnContainer>
+
+        <ColumnContainer>
+          <h3>Contact Info</h3>
+          <p>
+            {' '}
+            <StyledIcon icon={faMap} paddingRight="0.8rem" />
+            Address: 1234, Park Avenue, NY
+          </p>
+          <p>
+            {' '}
+            <StyledIcon icon={faPhone} paddingRight="0.8rem" />
+            +122-123-456
+          </p>
+          <p>
+            {' '}
+            <StyledIcon icon={faEnvelope} paddingRight="0.8rem" />
+            newsletter@gmail.com
+          </p>
+          <p>
+            {' '}
+            <StyledIcon icon={faClock} paddingRight="0.8rem" />
+            7:00am - 10:00pm
+          </p>
+        </ColumnContainer>
+
+        <ColumnContainer>
+          <h3>Newsletter</h3>
+          <p>Subscribe to get the latest updates</p>
+          <form action="">
+            <EmailInput type="email" placeholder="Enter your email" />
+            <Button>Subscribe</Button>
+          </form>
+        </ColumnContainer>
+      </GridContainer>
+    </FooterWrapper>
+  );
+};
