17684a05f6a05f9154716f0fbc5d1001677acebd
Zin Hoang <hoang.mantue@gmail.com>
2024-11-28 11:28:04 +1300

    Adjust section padding for smaller screens

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -11,7 +11,6 @@ export const GlobalStyle = createGlobalStyle`
   outline:none;
   border:none;
   text-decoration: none;
-  /* text-transform: capitalize; */
   transition: all .2s linear;
 }
 
@@ -20,7 +19,13 @@ a, a:visited {
 }
 
 html {
-  font-size: 62.5%; // need to figure out why this % and need to convert to rem
+  /* 
+    The default font-size for most browsers is 16px.
+    Setting font-size to 62.5% on the html element changes the base font size to 10px (62.5% of 16px).
+    With a base font size of 10px, 1rem equals 10px.
+    This makes it easier to calculate rem values. For example, 1.6rem equals 16px, 2rem equals 20px, etc. 
+  */
+  font-size: 62.5%;
   overflow-x: hidden;
   
   scroll-behavior: smooth;
@@ -55,6 +60,14 @@ body {
 
 section {
   padding: 0 10rem;
+
+  @media (max-width: 991px) {
+    padding: 0 5rem;
+  }
+
+  @media (max-width: 380px) {
+    padding: 0 2rem;
+  }
 }
 
 
