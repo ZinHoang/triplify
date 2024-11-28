9b954ea6b5e707e30194c6d8ae53d62d4bef83bc
Zin Hoang <hoang.mantue@gmail.com>
2024-11-28 15:28:44 +1300

    Enable Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Header } from './sections/header/Header';
 import { MainContent } from './sections/MainContent';
+import { Footer } from './sections/footer/Footer';
 
 const App = () => {
   return (
     <>
       <Header />;
       <MainContent />
-      {/* <Footer/> */}
+      <Footer />
     </>
   );
 };
