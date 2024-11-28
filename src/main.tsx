dc599e4b9f790931442cd581108fcf1d722d04ae
Zin Hoang <hoang.mantue@gmail.com>
2024-11-29 12:31:11 +1300

    Remove AOS init in main

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,7 @@ import App from './App.tsx';
 import { GlobalStyle } from './styles/GlobalStyle.ts';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/Theme.ts';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
 
-AOS.init();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
