e239567329376fa23ae0d844379db87c691d6f89
Zin Hoang <hoang.mantue@gmail.com>
2024-12-11 00:31:00 +1300

    use lazy loading image library

--- src/styles/sharedStyledComponents/Image.styles.ts
+++ src/styles/sharedStyledComponents/Image.styles.ts
@@ -1,9 +1,11 @@
-import styled from "styled-components";
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import styled from 'styled-components';
 
 export const ImageContainer = styled.div`
   width: 100%;
 `;
-export const Image = styled.img`
+
+export const Image = styled(LazyLoadImage)`
   height: 20rem;
   width: 100%;
   object-fit: cover;
