d8507590e2f8efeba8c061ec1f9f159c6e77ccc5
Zin Hoang <hoang.mantue@gmail.com>
2024-12-11 17:34:24 +1300

    Use react-awesome-reveal animation and remove other animation packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Header } from './sections/header/Header';
 import { MainContent } from './sections/MainContent';
 import { Footer } from './sections/footer/Footer';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
 
 const App = () => {
-  useEffect(() => {
-    AOS.init({ once: true });
-  }, []);
   return (
     <>
       <Header />;
