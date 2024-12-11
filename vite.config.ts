146e47d4845b8a4a37ac3f3db8a23643ee190004
Zin Hoang <hoang.mantue@gmail.com>
2024-12-12 00:05:40 +1300

    update config for Netlify deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  esbuild: {
+    logOverride: {
+      'react-is-declared-but-its-value-is-never-read': 'silent', // suppress React unused import warnings for Netlify deployment
+    },
+  },
+});
